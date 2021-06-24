12d59b181d609505f99e642d8c935f41da2963ff
terada0628 <ntpeanuts@yahoo.co.jp>
2021-06-24 14:32:19 +0000

    1continued

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#top'
+  root 'homes#top'
   get 'home/about' => 'homes#about'
   resources :books,only:[:create,:index,:show,:edit,:update,:destroy]
   resources :users,only:[:index,:show,:edit,:update]
