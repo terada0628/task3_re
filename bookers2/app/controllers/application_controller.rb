6017a2cc0684a8093623777a84d3aeb540dcbe64
terada0628 <ntpeanuts@yahoo.co.jp>
2021-06-25 14:43:12 +0000

    3continued

--- bookers2/app/controllers/application_controller.rb
+++ bookers2/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+before_action :configur_permitted_parameters,if: :devise_controller?
+
+protected
+
+def configure_permitted_parameters
+    devise_paramete_sanitizer.permit(:sign_up,keys:[:name])
+end
+
+
 end
