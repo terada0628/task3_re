e5f051e3967cbd7a139d0b6c1297801cc11ecc30
terada0628 <ntpeanuts@yahoo.co.jp>
2021-06-27 13:53:08 +0000

    finish

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   attachment :profile_image
   
   validates :name,uniqueness: true,length: {in: 2..20}
-  validates :introduction,length: {maximum: 20}
+  validates :introduction,length: {maximum: 50}
 
 end
