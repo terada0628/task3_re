e9c2bc83f5f642df24cbbb28af6d690d76371eb5
terada0628 <ntpeanuts@yahoo.co.jp>
2021-06-27 12:19:35 +0000

    5continued

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -3,8 +3,13 @@ class BooksController < ApplicationController
   def create
     @new_book = Book.new(book_params)
     @new_book.user_id = current_user.id
-    @new_book.save
-    redirect_to book_path(@new_book[:id])
+   if @new_book.save
+    redirect_to book_path(@new_book[:id]), notice: 'You have created book successfully.'
+   else
+    @user = current_user
+    @books = Book.all
+    render :index
+   end
   end
 
   def index
@@ -25,8 +30,10 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'You have updated book successfully.'
+    else render :edit
+    end
   end
 
   def destroy
