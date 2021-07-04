7dda4cf2652c6327f4e0d593552af3e51c215473
terada0628 <ntpeanuts@yahoo.co.jp>
2021-07-04 09:43:13 +0000

    phase2-4test1

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -22,4 +22,12 @@ class User < ApplicationRecord
       User.where('name LIKE ?', '%' + content + '%')
     end
   end
+  
+  # フォロー機能ここから
+  has_many :reverse_of_relationships, class_name: "Relatioship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  # フォロー機能
 end
